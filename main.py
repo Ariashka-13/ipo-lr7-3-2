fceb127584b88bc6fb2d6bdab4a7b0cdb0e67177
Ariashka <ariana0tsarova@gmail.com>
2024-12-04 15:58:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ import json
 with open("dump.json", "r", encoding="UTF-8") as file:
     data = json.load(file)
 
-n = int(input("Введите номер квалификации: "))
+n = input("Введите номер квалификации: ")
+found = False
 
 for skill in data:
     if skill.get("model") == "data.skill":
-        if skill["fields"].get("specialty") == n:
-            skill_code = skill["fields"].get("code")
+        if skill["fields"].get("code") == n:
             skill_title = skill["fields"].get("title")
-            
-            for profession in data:
-                if profession.get("model") == "data.specialty":
-                    specialty_code = profession["fields"].get("code")
-                    if specialty_code in skill_code:
-                        specialty_title = profession["fields"].get("title")
-                        specialty_educational = profession["fields"].get("c_type")
-                        
-            print("=============== Найдено ===============")
-            print(f"{specialty_code} >> Специальность '{specialty_title}' , {specialty_educational}")
-            print(f"{skill_code} >> Квалификация '{skill_title}'")
-            break
-else:
-    print("=============== Не Найдено ===============")
+            skill_specialty = skill["fields"].get("specialty")
+            for specialty in data:
+                if specialty.get("model") == "data.specialty":
+                    if skill_specialty == specialty.get("pk"):
+                        specialty_code = specialty["fields"].get("code")
+                        specialty_title = specialty["fields"].get("title")
+                        specialty_educational = specialty["fields"].get("c_type")
+                        print("=============== Найдено ===============")
+                        print(f"{specialty_code} >> Специальность '{specialty_title}' , {specialty_educational}")
+                        print(f"{n} >> Квалификация '{skill_title}'")
+                        found = True
+                        break
+            if found:
+                break        
+if not found:
+    print("=============== Не Найдено ===============")                
